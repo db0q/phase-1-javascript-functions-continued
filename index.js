a7af828252a1d22567ea180bd1e0fa2eddd8d258
theMstafa <mm22180481@gmail.com>
2022-10-28 18:27:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(argument){
+    if (argument == undefined){
+    return `This Saturday, I want to roller-skate!`
+    }
+    return `This Saturday, I want to ${argument}!`
+}
+const mondayWork = function (workType="go to the office"){
+ 
+    return `This Monday, I will ${workType}.`
+
+ }
+const wrapAdjective = function(star) {
+    return function(adjective) {
+      return `You are ${star}${adjective}${star}!`
+    }
+}
